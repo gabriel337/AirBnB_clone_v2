5c6a0ba77b65be6c2a72d8f2059200d990ae5e78
gabriel337 <g.david.lopezruiz@gmail.com>
2022-05-15 18:13:09 +0000

    clean up

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-
 """
 starts a Flask web application
 """
-
-
 from flask import Flask
-
-
 app = Flask(__name__)
 
 
@@ -17,4 +12,4 @@ def hello():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0")
